99e73e3e338f213eb5b9563c0a9ed60ffdaa4447
Germán Poo-Caamaño <gpoo@gnome.org>
2014-07-07 09:16:20 -0700

    iptcinfo: Fix typos in class methods

--- src/iptcinfo.py
+++ src/iptcinfo.py
@@ -972,7 +972,7 @@ class IPTCInfo(object):
     app0data = self.jpegSkipVariable(fh, app0data)
     if app0data is None:
       self.error = 'jpegSkipVariable failed'
-      self.log(error)
+      self.log(self.error)
       return None
 
     if ord(marker) == 0xe0 or not discardAppParts:
@@ -1186,7 +1186,7 @@ class IPTCInfo(object):
               tuple(map(ord, row) + [''.join(map(P, row))])
     return ered
 
-  def jpegDebugScan(filename):
+  def jpegDebugScan(self, filename):
     """Also very helpful when debugging."""
     assert isinstance(filename, basestring) and os.path.isfile(filename)
     fh = file(filename, 'wb')
